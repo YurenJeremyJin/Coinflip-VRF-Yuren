6a583adcb917cee976821a684a24a87a7ff8b2df
Jeremy Jin <jeremy@Jeremys-MBP.lan>
2024-02-25 17:02:02 +0100

    Compile Successful

--- src/Coinflip.sol
+++ src/Coinflip.sol
@@ -17,7 +17,7 @@ contract Coinflip is Ownable{
     constructor() Ownable(msg.sender) {}
 
     ///@notice Fund the VRF instance with **2** LINK tokens.
-    ///@return A boolean of whether funding the VRF instance with link tokens was successful or not
+    //@return A boolean of whether funding the VRF instance with link tokens was successful or not
     ///@dev use the address of LINK token contract provided. Do not change the address!
     ///@custom:attention In order for this contract to fund another contract, which tokens does it require to have before calling this function?
     ///                  What **additional** functions does this contract need to receive these tokens itself?
@@ -26,18 +26,18 @@ contract Coinflip is Ownable{
     }
 
     ///@notice user guess only ONE flip either a 1 or a 0.
-    ///@param a uint8 which is required to be 1 or 0
+    //@param a uint8 which is required to be 1 or 0
     ///@dev After validating the user input, store the user input in global mapping and fire off a request to the VRF instance
     ///@dev Then, store the requestid in global mapping
     function userInput(uint8 Guess) external {}
 
     ///@notice due to the fact that a blockchain does not deliver data instantaneously, in fact quite slowly under congestion, allow
     ///        users to check the status of their request.
-    ///@return a boolean of whether the request has been fulfilled or not
+    //@return a boolean of whether the request has been fulfilled or not
     function checkStatus() external view returns(bool){}
 
     ///@notice once the request is fulfilled, return the random result and check if user won
-    ///@return a boolean of whether the user won or not based on their input
+    //@return a boolean of whether the user won or not based on their input
     ///@dev request the randomWord that is returned. Here you need to check the VRFcontract to understand what type the random word is returned in
     ///@dev simply take the first result, or you can configure the VRF to only return 1 number, and check if it is even or odd. 
     ///     if it is even, the randomly generated flip is 0 and if it is odd, the random flip is 1
